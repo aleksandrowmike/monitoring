06206bf25dbc084e87d1d3d8ca0d3350668eb736
Mikhail Gorbachev <gor.mihal@gmail.com>
2020-04-18 20:07:17 +0300

    travis 6

--- api/tests/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require __DIR__ . '/../bootstrap/autoload.php';
-
-(new class() {
-    use \Tests\CreatesApplication;
-})->createApplication()[\Illuminate\Contracts\Console\Kernel]->call('migrate:fresh');
