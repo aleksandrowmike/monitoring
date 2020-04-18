a664bcd0233ff8593a082e25b0e513ab4db6ca17
Mikhail Gorbachev <gor.mihal@gmail.com>
2020-04-18 19:42:43 +0300

    Добавлен кофиг для travis 2

--- develop.sh
+++ develop.sh
@@ -1,9 +1,38 @@
 #!/usr/bin/env bash
 
+if [ -f ".docker-env" ]; then
+    . ./.docker-env
+fi
+
 export REDIS_PASSWORD=${REDIS_PASSWORD:-app}
 
-COMMAND="docker-compose -f docker-compose.base.yml $@"
+case "$1" in
+    "test-db")
+        shift 1
+        CONSOLE_COMMAND="migrate"
+        if [ "$1" == "-f" ]; then
+            CONSOLE_COMMAND="migrate:fresh"
+        fi
+        COMMAND="exec php php artisan $CONSOLE_COMMAND --force --database=test --seed"
+    ;;
+    "art")
+        shift 1
+        COMMAND="exec php php artisan $@"
+    ;;
+    "composer")
+        shift 1
+        COMMAND="run --rm php composer $@"
+    ;;
+    "")
+        COMMAND="ps"
+    ;;
+    *)
+        COMMAND="$@"
+    ;;
+esac
 
-echo "${COMMAND}";
+COMMAND="docker-compose -f docker-compose.base.yml ${COMMAND}"
+
+echo ${COMMAND};
 
 ${COMMAND}
